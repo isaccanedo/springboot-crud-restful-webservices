d2c965667c20c47d7e9be960b7f4598d7e8244ce
isaccanedo <isaccanedo@gmail.com>
2020-09-06 00:07:48 -0300

    Spring Boot, MariaDB, JPA, Hibernate Restful CRUD API

---
+++ src/test/java/br/com/isaccanedo/springboot/SpringbootCrudRestfulWebservicesApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.isaccanedo.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootCrudRestfulWebservicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
